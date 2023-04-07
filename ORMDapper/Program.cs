bb1acf958a90bddec4c801d66743f9edbc20ac59
Alec <alec.m.hancock@gmail.com>
2023-04-07 11:47:54 -0500

    ASsignment Complete, 2 departments created

--- ORMDapper/Program.cs
+++ ORMDapper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace ORMDapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORMDapper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentrepo = new DapperDepartmentRepository(conn);
+            departmentrepo.InsertDepartment("Department 88");
+
+            var departments = departmentrepo.GetAllDepartments();
+            foreach (var department in departments)
+            {
+                Console.WriteLine(department.DepartmentId);
+                Console.WriteLine(department.Name);
+                Console.WriteLine();
+            }
+
+            
         }
     }
 }
